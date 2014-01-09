ecb8932d23833c080062096620253a6550b1f3bd
Dan <ridethelightningg@gmail.com>
2014-01-09 07:46:41 +0100

    add winner. change warnings code. fix some small typos/variable names

--- Cats/plugin.py
+++ Cats/plugin.py
@@ -50,7 +50,7 @@ class Cats(callbacks.Plugin):
                 irc.error('The cats are already running!')
             else:
                 irc.reply('Starting Scattergories...\x02type !jcats to join\x02. Categories will be revealed in 30 seconds, then a letter will be chosen.', prefixNick=False)
-                irc.reply('Players will have 60 seconds to PM answers matching the categories starting with that letter to IPT.', prefixNick=False)
+                irc.reply('Players will have 90 seconds to PM answers matching the categories starting with that letter to IPT.', prefixNick=False)
                 irc.reply('For example, letter A, category 4, Math Terms, you could PM 4 addition. At the end of the round, you will receive points for unique answers.', prefixNick=False)
                 self.catsrunning = True
                 self.catsjoining = True
@@ -66,7 +66,7 @@ class Cats(callbacks.Plugin):
 
                 def chooseLetter():
                     self.letter = random.choice('ABCDEFGHIJKLMNOPRSTW')
-                    irc.reply('The letter for this round is... %s' % self.letter, prefixNick=False) 
+                    irc.reply('I rolled a d20 and the letter for this round is... %s' % self.letter, prefixNick=False) 
                     irc.reply('You have 90 seconds starting right meow!', prefixNick=False)
                     self.catsplaying = True
 
@@ -75,48 +75,53 @@ class Cats(callbacks.Plugin):
                             irc.sendMsg(ircmsgs.privmsg(x, y))
                         irc.sendMsg(ircmsgs.privmsg(x, 'Your answer must match the category and start with %s. Reply in format: # answer' % self.letter))
 
-                    schedule.addEvent(warningOne, time.time() + 30)
-                    schedule.addEvent(warningTwo, time.time() + 60)
-                    schedule.addEvent(warningThree, time.time() + 80)
+                    schedule.addEvent(doWarning, time.time() + 30, args=('One minute remaining!',))
+                    schedule.addEvent(doWarning, time.time() + 60, args=('30 seconds left!',))
+                    schedule.addEvent(doWarning, time.time() + 80, args=('10 seconds! Hurry!',))
                     schedule.addEvent(gameEnd, time.time() + 90)
 
-                def warningOne():
-                    irc.reply('One minute remaining!', prefixNick=False)
-
-                def warningTwo():
-                    irc.reply('30 seconds left!', prefixNick=False)
-
-                def warningThree():
-                    irc.remply('10 seconds! Hurry!', prefixNick=False)
+                def doWarning(s):
+                    irc.reply(s, prefixNick=False)
+                    for x in self.players:
+                        irc.sendMsg(ircmsgs.privmsg(x, s))
 
                 def gameEnd():
                     self.catsrunning = False
                     self.catsplaying = False
+                    for x in self.players:
+                        irc.sendMsg(ircmsgs.privmsg(x, 'Game over! Return to %s for results' % self.gamechan))
                     irc.reply('Game over!', prefixNick=False)
                     irc.reply('Results:', prefixNick=False)
 
                     m = max(len(x) for x in self.gamecard)
 
-                    for k, v in self.players.iteritems():
+                    for k, v in self.players.items():
                         v['m'] = max(len(v.get(i, '-')) for i in range(1,13))
 
                     for i in xrange(1,13):
-                        r = '%-*s (%s)' % (m, self.gamecard[i], ', '.join(['%s:%-*s' % (k, v['m'], v.get(i, '-')) for k,v in self.players.iteritems()]))
-                        answers = [x[i] for x in self.players.itervalues() if i in x]
+                        r = '%-*s (%s)' % (m, self.gamecard[i], ', '.join(['%s:%-*s' % (k, v['m'], v.get(i, '-')) for k,v in self.players.items()]))
+                        answers = [x[i] for x in self.players.values() if i in x]
                         r += ' +1 '
                         w = []
-                        for n,x in self.players.iteritems():
-                            if i in x and answers.count(x[i]) == 1:
-                                x['score'] += 1
-                                w.append(n)
+                        for k,v in self.players.items():
+                            if i in v and answers.count(v[i]) == 1:
+                                v['score'] += 1
+                                w.append(k)
                         if w:
                             r += ', '.join(w)
                         else:
                             r += 'nobody :('
                         irc.reply(r, prefixNick=False)
 
-                    for n,x in self.players.iteritems():
-                        irc.reply('Total score for %s: %s' % (n, x['score']), prefixNick=False)
+                    for k,v in self.players.items(): # announce all scores
+                        irc.reply('Total score for %s: %s' % (k, v['score']), prefixNick=False)
+
+                    # get highest score
+                    topscore = max(v['score'] for v in self.players.values())
+
+                    for k,v in self.players.items(): # announce winner
+                        if v['score'] == topscore:
+                            irc.reply('%s is winner with score of %s!' % (k, topscore), prefixNick=False)
 
                     self.players = {}
 
@@ -126,11 +131,11 @@ class Cats(callbacks.Plugin):
 
     def jcats(self, irc, msg, args):
         """docstring"""
-        if msg.args[0] == self.gamechan:
-            if self.catsjoining is True:
-                if msg.nick in self.players:
+        if msg.args[0] == self.gamechan: # check if in right channel
+            if self.catsjoining is True: # check if ok to join
+                if msg.nick in self.players: # check they havnt already joined
                     irc.error('You have already joined the round!')
-                else:
+                else: # if all is ok, we add them to players
                     self.players[msg.nick] = {'score': 0}
                     irc.replySuccess()
             else:
@@ -141,7 +146,7 @@ class Cats(callbacks.Plugin):
     def doPrivmsg(self, irc, msg):
         if self.catsplaying and msg.args[0].lower() == irc.nick.lower() and msg.nick in self.players:
             parts = msg.args[1].split(None, 1)
-            if len(parts) == 2 and parts[0].isdigit():
+            if len(parts) == 2 and parts[0].isdigit(): # syntax check on player's answer
                 i = int(parts[0])
                 if i > 0 and i < 13:
                     if parts[1][0].upper() == self.letter:
